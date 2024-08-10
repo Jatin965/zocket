0e732ae9b55dfe84a7ef832757837f921851f68b
Jatin Saini <jatin4762@gmail.com>
2024-08-10 23:16:58 +0530

    Added meta title and icon

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,19 @@ import { DndProvider } from "react-dnd";
 import "../styles/globals.css";
 
 import { HTML5Backend } from "react-dnd-html5-backend";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <DndProvider backend={HTML5Backend}>
+      <Head>
+        <title>Zocket - Run digital ads in 30 seconds</title>
+        <link
+          rel="icon"
+          type="image/png"
+          href="https://d273i1jagfl543.cloudfront.net/assets/animationFiles/zocket.svg"
+        ></link>
+      </Head>
       <Component {...pageProps} />
     </DndProvider>
   );
